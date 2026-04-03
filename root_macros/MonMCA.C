7f20e2505c65a47787863aa3ebde3e0643a0cb58
Kentaro Miuchi <miuchi@panda.kobe-u.ac.jp>
2026-04-03 15:09:45 +0900

    analysis macros update

--- root_macros/MonMCA.C
+++ root_macros/MonMCA.C
@@ -8,16 +8,17 @@
 
 //int MonMCA(){
 int MonMCA(string tmp_filename){
-	const int MCACh = 8191;
+	const int MCACh = 8192;
 	const int header = 12;
 	const int live_line=7;
 	const int real_line=8;
+	const int th_line=4;
 	//const char tmp_filename[32]="tmp.mca";
 	TCanvas *c = new TCanvas("c","",800,600);
 
 	double val;
 	string str;
-	string str_live,str_real;
+	string str_live,str_real,str_th;
 	//ifstream ifs("718.dat");
 	//ifstream ifs("794.dat");
 	while(1){
@@ -25,17 +26,11 @@ int MonMCA(string tmp_filename){
 	  //ifstream ifs("tmp.mca");
 	  ifstream ifs(tmp_filename.c_str());
 	  for(int ich=0; ich<header; ++ich){
-	  //		ifs >> sval;	
 	    getline(ifs,str);
-	    if (ich==live_line){
-	      //cerr <<"live time: "<< str<<" sec"<<endl;
-	      str_live=str;
-	      //cerr <<"live time: "<< str.substr(str.find_last_of(" "),6)<<" sec"<<endl;
-	    }
-	    if (ich==real_line){
-	      str_real=str;
-	      //cerr <<"real time: "<< str<<" sec"<<endl;
-	    }
+	    if (ich==live_line)	      str_live=str;
+	    if (ich==real_line)	      str_real=str;
+	    if (ich==th_line)	      str_th=str;
+
 	    //cerr << str<<endl;
 	  }
 	  for(int ich=0; ich<MCACh; ++ich){
@@ -46,11 +41,14 @@ int MonMCA(string tmp_filename){
 	  TText *tn = new TText(0.6*MCACh, 0.8*hist->GetMaximum(),tmp_filename.c_str());
 	  TText *tl = new TText(0.6*MCACh, 0.7*hist->GetMaximum(), str_live.c_str());
 	  TText *tr= new TText(0.6*MCACh, 0.6*hist->GetMaximum(), str_real.c_str());
+	  TText *tth= new TText(0.6*MCACh, 0.5*hist->GetMaximum(), str_th.c_str());
 	  //	  tl->SetTextAlign(22);
 	  tn->SetTextSize(0.04);
 	  tl->SetTextSize(0.04);
+	  tth->SetTextSize(0.04);
 	  tr->SetTextSize(0.04);
 	  hist->Draw();
+	  tth->Draw(); 
 	  tn->Draw(); 
 	  tl->Draw(); 
 	  tr->Draw(); 
